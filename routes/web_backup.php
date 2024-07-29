c3f720736e106825c185bf48835525ab295a6105
ono-30 <mio.run.trip@gmail.com>
2024-07-29 10:52:00 +0900

    課題提出

--- routes/web_backup.php
+++ routes/web_backup.php
@@ -1,7 +1,7 @@
 <?php
 
-use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
+
 // ルーティングを設定するコントローラを宣言する
 use App\Http\Controllers\PostController;
 
@@ -16,29 +16,13 @@ use App\Http\Controllers\PostController;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
-
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
-
-Route::middleware('auth')->group(function () {
-    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-});
-
-require __DIR__ . '/auth.php';
-
 Route::get('/posts', [PostController::class, 'index']);
 
 // '/posts/create'にGETメソッドでアクセス（HTTPリクエストを送信）したときにPostControllerのcreateアクションが実行される
-Route::get('/posts/create', [PostController::class, 'create'])->middleware('auth');
+Route::get('/posts/create', [PostController::class, 'create']);
 
 // '/posts/store'にPOSTメソッドでアクセス（HTTPリクエストを送信）したときにPostControllerのstoreアクションが実行される
-Route::post('/posts/store', [PostController::class, 'store'])->name('posts.store')->middleware('auth');
+Route::post('/posts/store', [PostController::class, 'store'])->name('posts.store');
 
 // 「/posts/1」のように可変の値を含むURLにアクセスした際、PostControllerのshowアクションが実行されるようルーティングを設定
 Route::get('/posts/{id}', [PostController::class, 'show']);
